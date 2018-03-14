ea2f644690e527eaa9c266fdf70fee25710f4b76
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-03-14 23:41:30 +0530

    Print two Lines on the same line

---
+++ ex7.py
@@ -0,0 +1,28 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) #multiply the string 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+#Here after the comma a new variable 'end' is defined which is a `space`
+#And it prints "Cheese" & "Burger" on the same line
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+
+print("") #prints an empty line
+print("Let's try printing ", end = '')
+#by using `end = '' ` we can print the next line on this line
+print("this on the same line.")
