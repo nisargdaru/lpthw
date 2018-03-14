bf2d7a90caf594f37e8722c4293e361afd41d013
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-03-14 20:04:00 +0530

    Define 'carpool' variable.

--- ex4.py
+++ ex4.py
@@ -1,2 +1,18 @@
 cars = 100
-print("There are" + str(cars) + "cars available.")
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+#carpool_capacity (variable) was not defined.
+
+print("There are" , cars,  "cars available.")  #seperated with comma
+
+print("There are only", drivers, "drivers available.")
+print("There will be" , cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+
+print("We have", passengers, "to carpool today.")
+print("we need to put about", average_passengers_per_car, "in each car.")
