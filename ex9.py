734480d6ea6a28dbda9c5b57adf79c41750e8813
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-03-18 15:43:03 +0530

    Print multiple lines

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon TUe Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug\n"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+print(""" #this displays a new line if left empty
+There's something going on here.
+With that three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.""")
