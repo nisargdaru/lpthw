f0031ae889f86c978c20d97c48e1d133b68e4ace
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-03-18 16:16:52 +0530

    Use some `escape sequences`

---
+++ ex10.py
@@ -0,0 +1,14 @@
+cat1 = "\tI'm tabbed in."
+cat2 = "I'm split\non a line."
+cat3 = "I'm a \\cat\\"
+cat4 = '''
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+'''
+
+print(cat1)
+print(cat2)
+print(cat3)
+print(cat4)
