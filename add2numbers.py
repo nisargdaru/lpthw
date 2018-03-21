8bb3c79e656b490f43924226e8c057966a8383f8
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-03-21 22:08:56 +0530

    Write a program to add two numbers

---
+++ add2numbers.py
@@ -0,0 +1,13 @@
+print("A program to add two numbers")
+print("")
+
+print("Enter the first number: " ,end = '')
+x = float(input())
+
+print("Enter the second number: " ,end = '')
+y = float(input())
+
+print("")
+
+add = x + y
+print(f"The addition of the two numbers is : {add}")
