80ce431dc1f8fc54cfd3e1a9c8ade80e8f7e0bb2
nisargdaru <31799441+nisargdaru@users.noreply.github.com>
2018-04-01 11:43:23 +0530

    No idea

---
+++ crash_course_2.py
@@ -0,0 +1,72 @@
+codename = "scorpion"
+print(codename.title())
+
+team = "SCORPION"
+print(team)
+
+A_name = "   Alpha "
+print(A_name)
+print(A_name.strip())
+print(A_name.strip().upper())
+
+B = 'beta'
+C = 'theta'
+D = 'delta'
+print("/tPython")
+Hello = 'Hello'
+World = 'World'
+line = "        "
+print(Hello + line + World)
+
+
+#Using different methods for changing the case
+print(D.upper())
+print(B.lower())
+print(C.title())
+
+#Adding Whitespace to STRINGS with Tab or New-line
+print("The languages I'm currently interested in learning are:\n\tJava\n\tPython")
+
+
+#Stripping Whitespace from Python can be done by using the following methods
+j = " sl"
+f = "sr "
+l = " slr "
+print(j)
+print(f)
+print(l)
+print("     ")
+print(j.lstrip())
+print(f.rstrip())
+print(l.strip())
+
+#To-do activities
+name = "Eric"
+message = " would you like to learn some python today?"
+print('Albert Einstien once said that "A person who have never made mistakes, is the person who have never tried anything" ')
+
+
+roll_one = "\tone"
+roll_two = "\ntwo"
+roll_three = "\nthree"
+print("  ")
+print(roll_one)
+print(roll_two)
+print(roll_three)
+
+print(roll_one.lstrip())
+print(roll_two.rstrip())
+print(roll_three.strip())
+
+print("\n")
+
+
+print(5+3)
+print(9-1)
+print(2*4)
+print(16//2)
+print("\n")
+
+fav_num = 9
+print("My favourite number is" + " " +str(fav_num)+ '.' )
+import this
